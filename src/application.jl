d8766e01605468483445b4b85aac007e20c3cf16
serenity4 <cedric.bel@hotmail.fr>
2024-10-15 14:28:44 +0200

    Don't use Box explicitly

--- src/application.jl
+++ src/application.jl
@@ -1,25 +1,25 @@
 function start_app()
-  @set_name side_panel = Rectangle(Box(P2(0.4, 0.9)), RGB(0.01, 0.01, 0.01))
+  @set_name side_panel = Rectangle((0.8, 1.8), RGB(0.01, 0.01, 0.01))
 
-  @set_name save_button = Button(Box(P2(0.15, 0.05)); text = Text("Save")) do
+  @set_name save_button = Button((0.3, 0.1); text = Text("Save")) do
     save_button.background.color = rand(RGB{Float32})
   end
 
   # File menu.
-  file_menu_head = Button(() -> collapse!(file_menu), Box(P2(0.15, 0.05)); text = Text("File"))
-  file_menu_item_1 = MenuItem(Text("New file"), Box(P2(0.15, 0.05))) do
+  file_menu_head = Button(() -> collapse!(file_menu), (0.3, 0.1); text = Text("File"))
+  file_menu_item_1 = MenuItem(Text("New file"), (0.3, 0.1)) do
     save_button.background.color = RGB{Float32}(0.1, 0.3, 0.2)
   end
-  file_menu_item_2 = MenuItem(Text("Open..."), Box(P2(0.15, 0.05))) do
+  file_menu_item_2 = MenuItem(Text("Open..."), (0.3, 0.1)) do
     save_button.background.color = RGB{Float32}(0.3, 0.2, 0.1)
   end
-  file_menu_item_3 = MenuItem(Anvil.exit, Text("Exit"), Box(P2(0.15, 0.05)), 'x')
+  file_menu_item_3 = MenuItem(Anvil.exit, Text("Exit"), (0.3, 0.1), 'x')
   @set_name file_menu = Menu(file_menu_head, [file_menu_item_1, file_menu_item_2, file_menu_item_3], 'F')
   add_constraint(attach(at(file_menu, :corner, :top_left), at(app.windows[app.window], :corner, :top_left)))
 
   # Edit menu.
-  edit_menu_head = Button(() -> collapse!(edit_menu), Box(P2(0.15, 0.05)); text = Text("Edit"))
-  edit_menu_item_1 = MenuItem(Text("Regenerate"), Box(P2(0.15, 0.05))) do
+  edit_menu_head = Button(() -> collapse!(edit_menu), (0.3, 0.1); text = Text("Edit"))
+  edit_menu_item_1 = MenuItem(Text("Regenerate"), (0.3, 0.1)) do
   end
   @set_name edit_menu = Menu(edit_menu_head, [edit_menu_item_1], 'E')
   add_constraint(attach(at(edit_menu, :corner, :top_left), at(file_menu, :corner, :top_right)))
@@ -31,9 +31,9 @@ function start_app()
   add_constraint(attach(at(at(side_panel, :center), P2(-0.4, 0.0)), at(at(app.windows[app.window], :center), P2(0.5, 0.0))))
 
   @set_name node_name_text = Text("Name")
-  @set_name node_name_value = Rectangle(Box(P2(0.05, 0.02)), RGB(0.2, 0.2, 0.2))
+  @set_name node_name_value = Rectangle((0.1, 0.04), RGB(0.2, 0.2, 0.2))
   @set_name node_color_text = Text("Color")
-  @set_name node_color_value = Rectangle(Box(P2(0.05, 0.02)), RGB(0.3, 0.2, 0.9))
+  @set_name node_color_value = Rectangle((0.1, 0.04), RGB(0.3, 0.2, 0.9))
   @set_name node_hide_text = Text("Hide")
   @set_name node_hide_value = Checkbox(_ -> nothing)
   left_column = EntityID[
