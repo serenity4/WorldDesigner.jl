981a688b0d85b5f8d5a60d32f0a3c1a9018753c4
serenity4 <cedric.bel@hotmail.fr>
2024-11-28 16:50:24 +0100

    Split application logic into its own directory

--- src/WorldDesigner.jl
+++ src/WorldDesigner.jl
@@ -8,8 +8,10 @@ const Optional{T} = Union{T, Nothing}
 const FilePath = String
 
 include("characters.jl")
-include("state.jl")
-include("application.jl")
+
+include("application/state.jl")
+include("application/main.jl")
+include("application/characters.jl")
 
 function __init__()
   APPLICATION_DIRECTORY[] = joinpath(dirname(@__DIR__))
