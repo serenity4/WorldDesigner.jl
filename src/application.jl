03d1f47792b56f4ba046372e7fffc8783bfc0da8
serenity4 <cedric.bel@hotmail.fr>
2024-11-28 16:47:29 +0100

    Namespace widgets, mark background as opaque

--- src/application.jl
+++ src/application.jl
@@ -7,7 +7,7 @@ function start_app(state::ApplicationState)
   left_tabs = EntityID[characters_tab, places_tab, events_tab]
 
   window = app.windows[app.window]
-  @set_name central_panel = Rectangle((1, 1), "parchment-background-1.jpg", ImageParameters(tiled = true, scale = 0.03))
+  @set_name central_panel = Rectangle((1, 1), "parchment-background-1.jpg", ImageParameters(tiled = true, scale = 0.03, is_opaque = true))
   pin(central_panel, :left, at(window, :left); offset = 6)
   pin(central_panel, :right, at(window, :right))
   pin(central_panel, :bottom, at(window, :bottom))
@@ -35,9 +35,10 @@ function start_app(state::ApplicationState)
 end
 
 function add_left_tab(state, name)
-  @set_name tab = Rectangle("tab-left.png", ImageParameters(scale = 6))
+  namespace = "tabs"
+  @set_name namespace tab = Rectangle("tab-left.png", ImageParameters(scale = 6))
   color = state.active_tab == name ? :red : :black
-  @set_name text = Text(styled"{$color:$name}"; font = "MedievalSharp", size = 0.7)
+  @set_name namespace text = Text(styled"{$color:$name}"; font = "MedievalSharp", size = 0.7)
   put_behind(tab, text)
   observe!(state, :active_tab) do previous_tab, active_tab
     previous_tab == name && (text.value = styled"{black:$name}")
@@ -53,15 +54,17 @@ function generate_active_tab(state::ApplicationState)
 end
 
 function character_list_entry(info::CharacterInfo)
-  name = Text(styled"{black:$(info.name)}"; font = "MedievalSharp", size = 0.7)
-  @set_name icon = character_icon(info.portrait)
-  background = Rectangle((1, 1), RGB(0.7, 0.4, 0.1))
-  put_behind(background, icon)
+  namespace = "characters/$(info.name)"
+  @get_widget central_panel
+  @set_name namespace name = Text(styled"{black:$(info.name)}"; font = "MedievalSharp", size = 0.7)
+  @set_name namespace icon = character_icon(info.portrait)
+  @set_name namespace background = Rectangle((1, 1), RGB(0.7, 0.4, 0.1))
   place_after(name, icon; spacing = 1.0)
+  put_behind(background, icon)
   put_behind(background, name)
   pin(background, :top_left, at(icon, :top_right))
   pin(background, :bottom_left, at(icon, :bottom_right))
-  pin(background, :right, at(get_widget(:central_panel), :right); offset = -2)
+  pin(background, :right, at(central_panel, :right); offset = -2)
   Group(name, icon, background)
 end
 
