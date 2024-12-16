7ef689119b52d699a968c7aeee578b8c930e5b65
serenity4 <cedric.bel@hotmail.fr>
2024-12-16 22:46:50 +0100

    Add character node dragging

--- src/application/world_graph.jl
+++ src/application/world_graph.jl
@@ -15,7 +15,31 @@ function add_character_nodes(graph::WorldGraph)
   for character in graph.characters
     @set_name namespace icon = character_icon(character.info.illustration)
     set_geometry(icon, (size, size))
-    place(icon, central_panel |> at(character.coordinates))
+    coordinates = Ref(character.coordinates)
+    place(icon, central_panel |> at(coordinates))
+    let origin = Ref(P2(0, 0)), last_displacement = Ref(P2(0, 0))
+      add_callback(icon, DRAG, BUTTON_PRESSED; drag_threshold = 0.1) do input
+        displace_when_dragging!(coordinates, origin, last_displacement, icon, input)
+        input.type === DRAG || return
+        i = findfirst(node -> node.info == character.info, graph.characters)
+        isnothing(i) && return
+        node = graph.characters[i]
+        graph.characters[i] = @set node.coordinates = coordinates[]
+      end
+    end
+  end
+end
+
+function displace_when_dragging!(location::Ref, origin::Ref, last_displacement::Ref, object, input::Input)
+  if is_left_click(input)
+    origin[] = location[]
+    last_displacement[] = origin[]
+  elseif input.type === DRAG
+    target, event = input.drag
+    displacement = event.location .- input.source.event.location
+    segment = displacement .- last_displacement[]
+    last_displacement[] = SVector(displacement)
+    location[] = origin[] .+ displacement
   end
 end
 
