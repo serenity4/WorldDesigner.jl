2e83224e78dac0217de653014580eea2c93db7ad
serenity4 <cedric.bel@hotmail.fr>
2025-01-25 14:07:46 +0100

    Add character info frame stub

--- src/application/world_graph.jl
+++ src/application/world_graph.jl
@@ -12,6 +12,7 @@ function add_character_nodes(graph::WorldGraph)
   @get_widget central_panel
   namespace = "world_graph/character_nodes"
   (; size) = graph.character_options
+  (; root) = app.systems.event.ui
   for character in graph.characters
     @set_name namespace icon = character_icon(character.info.illustration)
     set_geometry(icon, FilledCircle(size/2))
@@ -21,24 +22,52 @@ function add_character_nodes(graph::WorldGraph)
       add_callback(icon, DRAG, BUTTON_PRESSED; drag_threshold = 0.1) do input
         displace_when_dragging!(coordinates, origin, last_displacement, icon, input)
         input.type === DRAG || return
-        i = findfirst(node -> node.info == character.info, graph.characters)
-        isnothing(i) && return
-        node = graph.characters[i]
+        i, node = find_node(graph, character)
         graph.characters[i] = @set node.coordinates = coordinates[]
       end
+      show_summary_callback = Ref{Optional{InputCallback}}(nothing)
+      hide_summary_callback = Ref{InputCallback}()
       add_callback(icon, POINTER_ENTERED) do input
-        reuse_interaction_set(:summary_on_hover) do
-          # TODO: Add background image.
-          @set_name namespace frame = Rectangle((5, 3), RGB(1, 0, 0))
-          place(at(frame, :bottom_left), at(icon, :top_right))
-          set_z(frame, Inf)
+        show_summary_callback[] = add_callback(root, KEY_PRESSED) do input
+          in(input.event.key_event.key_name, (:LCTL, :RCTL)) || return
+          add_character_info_frame!(hide_summary_callback, graph, character, icon, namespace)
         end
+        in(CTRL_MODIFIER, input.event.pointer_state.modifiers) && add_character_info_frame!(hide_summary_callback, graph, character, icon, namespace)
       end
       add_callback(icon, POINTER_EXITED) do input
-        reuse_interaction_set(() -> nothing, :summary_on_hover)
+        remove_callback(root, show_summary_callback[])
+        show_summary_callback[] = nothing
       end
     end
   end
+end
+
+function add_character_info_frame!(hide_summary_callback, graph, character, icon, namespace)
+  (; root) = app.systems.event.ui
+  reuse_interaction_set(:summary_on_hover) do
+    _, node = find_node(graph, character)
+    isnothing(node) && return
+    (; info) = node
+    parameters = ImageParameters(is_opaque = true, mode = ImageModeCropped())
+    @set_name namespace background = Rectangle((10, 10sqrt(2)), texture_file("parchment-background-3"), parameters)
+    @set_name namespace name = Text(styled"{black:$(info.name)}"; font = "MedievalSharp", size = 1.0)
+
+    place(at(background, :bottom_left), at(icon, :top_right))
+    # XXX: Putting the background or name in front or behind of one another
+    # wrongly leads to one or none of them being rendered!
+    place(at(name, :top), at(background, :top) |> at(-0.5))
+  end
+  hide_summary_callback[] = add_callback(root, KEY_RELEASED) do input
+    in(input.event.key_event.key_name, (:LCTL, :RCTL)) || return
+    reuse_interaction_set(() -> nothing, :summary_on_hover)
+    remove_callback(root, hide_summary_callback[])
+  end
+end
+
+function find_node(graph, character)
+  i = findfirst(node -> node.info == character.info, graph.characters)
+  isnothing(i) && return 0, nothing
+  i, graph.characters[i]
 end
 
 function displace_when_dragging!(location::Ref, origin::Ref, last_displacement::Ref, object, input::Input)
